c770abf50e5d8313239da814cafcbfa9c4a812af
Денис <33393393+hashmoker@users.noreply.github.com>
2018-12-20 17:44:16 +0300

    Add files via upload

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,9 @@ Auth::routes();
 
 Route::get('/admin', 'AdminController@dashboard')->name('dashboard');
 Route::resource('/admin/products', 'ProductController');
+Route::resource('/shop', 'ShopController');
 Route::resource('/admin/users', 'UserController');
-Route::get('/', function (){
+Route::get('/feedback', function (){
 return view ('form');
 
 });
